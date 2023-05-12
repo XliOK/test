483679ac3860189cefb72cf771871c2eeb23aae9
XliOK <130830285+XliOK@users.noreply.github.com>
2023-05-12 16:15:58 +0800

    Update acf.py

--- acf.py
+++ acf.py
@@ -245,6 +245,12 @@ class SteamCMD:
                     print(f"{manifest} was written successfully!")
             else:
                 print('Unknown error from SteamCMD:')
+                log_file_path = "/home/runner/Steam/logs/stderr.txt"
+                if os.path.exists(log_file_path):
+                    with open(log_file_path, "r") as file:
+                    print(file.read())
+                else:
+                    print(f"Log file {log_file_path} does not exist.")
                 print(output_app_ids_data)
         else:
             print('You have not entered any appId!')
