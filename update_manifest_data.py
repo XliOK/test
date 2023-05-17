4abe66558303f3d047ba9b19a3d5d207e1a83a77
XliOK <130830285+XliOK@users.noreply.github.com>
2023-05-17 08:45:20 +0800

    Update update_manifest_data.py

--- update_manifest_data.py
+++ update_manifest_data.py
@@ -98,7 +98,7 @@ if __name__ == "__main__":
     all_branches = list(repo.get_branches())
     check_remaining_count(github)
     
-    ...
+    '''
     # 设置最大线程数，根据您的实际情况进行调整
     max_threads = 10
 
@@ -109,5 +109,5 @@ if __name__ == "__main__":
     # 等待所有线程完成
     for future in futures:
         future.result()
-    ...
+    '''
 
